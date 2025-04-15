2ad3f2bb852f59497d8718d938d967ac67bd2be4
manoj <manojreddy2404@gmail.com>
2025-04-15 12:06:58 +0530

    Learning Python for DevOps

---
+++ Variables.py/ex1.py
@@ -0,0 +1,21 @@
+# Strings
+
+mystring = "one"
+print(mystring)
+
+MyString = "Dude im a string "
+print(MyString)
+
+# Floating Pointig Numbers 
+
+myfloat = 33.345  #Floating can be written in different ways 
+print(myfloat)
+
+Float = 34.43
+x = 34.04
+z = x+Float
+print(z)
+
+Float = 34.43
+x = 34.04
+print(x+Float )
